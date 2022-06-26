aeab52966442b39d8153c7c79842fecc75d43ba4
Your Name <you@example.com>
2022-06-26 05:43:09 +0000

    aosp

---
+++ device/motorola/titan/BoardConfig.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common msm8226-common
+-include device/motorola/msm8226-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/motorola/titan
+
+# Vendor Unification Init
+TARGET_INIT_VENDOR_LIB := libinit_titan
+TARGET_RECOVERY_DEVICE_MODULES := libinit_titan
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := titan,titan_umts,titan_udstv,titan_umtsds,titan_retaildsds,XT1068,XT1064,XT1063,XT1069
+
+# Board
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# Kernel
+TARGET_KERNEL_CONFIG := titan_defconfig
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10444800
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10526720
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1157627904
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5912772608
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 560070656
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# inherit from the proprietary version
+-include vendor/motorola/titan/BoardConfigVendor.mk
---
+++ device/motorola/titan/init/init_titan.cpp
@@ -0,0 +1,108 @@
+/*
+   Copyright (c) 2013, The Linux Foundation. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include "vendor_init.h"
+#include "property_service.h"
+#include "log.h"
+#include "util.h"
+
+void gsm_properties();
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void vendor_load_properties()
+{
+    std::string platform = property_get("ro.board.platform");
+    if (platform != ANDROID_TARGET)
+        return;
+
+    std::string radio = property_get("ro.boot.radio");
+
+    property_override("ro.product.model", "Moto G 2014");
+
+    if (radio == "0x1") {
+        /* XT1063 */
+        property_override("ro.product.device", "titan_umts");
+        property_override("ro.build.description", "titan_retuglb-user 6.0 MPB24.65-34 31 release-keys");
+        property_override("ro.build.fingerprint", "motorola/titan_retuglb/titan_umts:6.0/MPB24.65-34/31:user/release-keys");
+        property_override("ro.build.product", "titan_umts");
+        property_set("ro.mot.build.customerid", "retusa_glb");
+        property_set("ro.telephony.default_network", "0");
+        property_set("persist.radio.multisim.config", "");
+    } else if (radio == "0x5") {
+        /*XT1068 */
+        property_override("ro.product.device", "titan_umtsds");
+        property_override("ro.build.description", "titan_retaildsds-user 6.0 MPB24.65-34 31 release-keys");
+        property_override("ro.build.fingerprint", "motorola/titan_retaildsds/titan_umtsds:6.0/MPB24.65-34/31:user/release-keys");
+        property_override("ro.build.product", "titan_umtsds");
+        property_set("ro.mot.build.customerid", "retaildsdsall");
+        property_set("ro.telephony.default_network", "0,1");
+        property_set("ro.telephony.ril.config", "simactivation,sim2gsmonly");
+        property_set("persist.radio.multisim.config", "dsds");
+        property_set("persist.radio.dont_use_dsd", "true");
+        property_set("persist.radio.plmn_name_cmp", "1");
+    } else if (radio == "0x6") {
+        /* XT1064 */
+        property_override("ro.product.device", "titan_umts");
+        property_override("ro.build.description", "titan_retuaws-user 6.0 MPB24.65-34 31 release-keys");
+        property_override("ro.build.fingerprint", "motorola/titan_retuaws/titan_umts:6.0/MPB24.65-34/31:user/release-keys");
+        property_override("ro.build.product", "titan_umts");
+        property_set("ro.mot.build.customerid", "retusa_aws");
+        property_set("ro.telephony.default_network", "0");
+        property_set("persist.radio.multisim.config", "");
+    } else if (radio == "0x7") {
+        /* XT1069 */
+        property_override("ro.product.device", "titan_udstv");
+        property_override("ro.build.description", "titan_retbr_dstv-user 6.0 MPB24.65-34 31 release-keys");
+        property_override("ro.build.fingerprint", "motorola/titan_retbr_dstv/titan_udstv:6.0/MPB24.65-34/31:user/release-keys");
+        property_override("ro.build.product", "titan_udstv");
+        property_set("ro.mot.build.customerid", "retbr");
+        property_set("ro.telephony.default_network", "0,1");
+        property_set("ro.telephony.ril.config", "simactivation,sim2gsmonly");
+        property_set("persist.radio.multisim.config", "dsds");
+        property_set("persist.radio.dont_use_dsd", "true");
+        property_set("persist.radio.plmn_name_cmp", "1");
+    }
+    std::string device = property_get("ro.product.device");
+    INFO("Found radio id %s setting build properties for %s device\n", radio.c_str(), device.c_str());
+}
